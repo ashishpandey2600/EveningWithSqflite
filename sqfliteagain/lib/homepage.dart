c0f05c6ee89919332c79380826aebd78f13a96e6
ashishpandey2600 <ashishpandey@Ashishs-MacBook-Pro.local>
2023-04-08 20:22:56 +0530

    added new file

--- sqfliteagain/lib/homepage.dart
+++ sqfliteagain/lib/homepage.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:sqfliteagain/dbhelper.dart';
@@ -39,27 +38,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(title: Text("Hola")),
       body: Column(children: [
-        TextField(
-          controller: titleController,
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        TextField(
-          controller: ageController,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        TextField(
-          controller: descriptionController,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        TextField(
-          controller: emailController,
-        ),
+        
       ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -70,55 +49,7 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+  
 }
 
-class Shownotes extends StatefulWidget {
-  const Shownotes({super.key});
 
-  @override
-  State<Shownotes> createState() => _ShownotesState();
-}
-
-class _ShownotesState extends State<Shownotes> {
-  NotesModel? notesModel;
-  late Future<List<NotesModel>> notesList;
-  DbHelper? dbHelper;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    dbHelper = DbHelper();
-    loadData();
-  }
-
-  loadData() async {
-    notesList = dbHelper!.getNotesList();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Notes"),
-        ),
-        body: Column(
-          children: [
-            FutureBuilder(
-                future: notesList,
-                builder: (context, AsyncSnapshot<List<NotesModel>> snapshot) {
-                  return ListView.builder(itemCount: snapshot.data?.length,
-                    itemBuilder: (context, index) {
-                    return Card(
-                        child: ListTile(
-                      contentPadding: EdgeInsets.all(0),
-                      title: Text(
-                        snapshot.data![index].title.toString(),
-                      ),
-                    ));
-                  });
-                })
-          ],
-        ));
-  }
-}
